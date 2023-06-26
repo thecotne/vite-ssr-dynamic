500e2ca63347ed410d26f26b1f3236502263d804
Tsotne Nazarashvili <cotne.95@gmail.com>
2023-06-26 20:20:25 +0400

    init

--- renderer/PageShell.tsx
+++ renderer/PageShell.tsx
@@ -1,45 +1,80 @@
-import React from 'react'
-import logo from './logo.svg'
-import { PageContextProvider } from './usePageContext'
-import type { PageContext } from './types'
-import './PageShell.css'
-import { Link } from './Link'
+import React from "react";
+import logo from "./logo.svg";
+import { PageContextProvider } from "./usePageContext";
+import type { PageContext } from "./types";
+import "./PageShell.css";
+import { Link } from "./Link";
 
-export { PageShell }
+export { PageShell };
 
-function PageShell({ children, pageContext }: { children: React.ReactNode; pageContext: PageContext }) {
+import { DynamicContextProvider, DynamicWidget } from "@dynamic-labs/sdk-react";
+
+// 1. Import the DynamicWagmiConnector
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+
+function PageShell({
+  children,
+  pageContext,
+}: {
+  children: React.ReactNode;
+  pageContext: PageContext;
+}) {
   return (
     <React.StrictMode>
       <PageContextProvider pageContext={pageContext}>
-        <Layout>
-          <Sidebar>
-            <Logo />
-            <Link className="navitem" href="/">
-              Home
-            </Link>
-            <Link className="navitem" href="/about">
-              About
-            </Link>
-          </Sidebar>
-          <Content>{children}</Content>
-        </Layout>
+        <DynamicContextProvider
+          settings={{
+            environmentId: "e8aee26c-efbc-4e28-9407-d990537a4a1c",
+          }}
+        >
+          <DynamicWagmiConnector
+            evmNetworks={[
+              {
+                blockExplorerUrls: ["https://etherscan.io/"],
+                chainId: 1,
+                chainName: "Ethereum Mainnet",
+                iconUrls: ["https://app.dynamic.xyz/assets/networks/eth.svg"],
+                nativeCurrency: { decimals: 18, name: "Ether", symbol: "ETH" },
+                networkId: 1,
+                privateCustomerRpcUrls: ["https://mainnet.infura.io/v3/xxxx"],
+                rpcUrls: ["https://cloudflare-eth.com"],
+                vanityName: "Ethereum",
+              },
+            ]}
+          >
+            <DynamicWidget />
+
+            <Layout>
+              <Sidebar>
+                <Logo />
+                <Link className="navitem" href="/">
+                  Home
+                </Link>
+                <Link className="navitem" href="/about">
+                  About
+                </Link>
+              </Sidebar>
+              <Content>{children}</Content>
+            </Layout>
+          </DynamicWagmiConnector>
+        </DynamicContextProvider>
       </PageContextProvider>
     </React.StrictMode>
-  )
+  );
 }
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div
       style={{
-        display: 'flex',
+        display: "flex",
         maxWidth: 900,
-        margin: 'auto'
+        margin: "auto",
       }}
     >
       {children}
     </div>
-  )
+  );
 }
 
 function Sidebar({ children }: { children: React.ReactNode }) {
@@ -48,15 +83,15 @@ function Sidebar({ children }: { children: React.ReactNode }) {
       style={{
         padding: 20,
         flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        lineHeight: '1.8em'
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        lineHeight: "1.8em",
       }}
     >
       {children}
     </div>
-  )
+  );
 }
 
 function Content({ children }: { children: React.ReactNode }) {
@@ -65,13 +100,13 @@ function Content({ children }: { children: React.ReactNode }) {
       style={{
         padding: 20,
         paddingBottom: 50,
-        borderLeft: '2px solid #eee',
-        minHeight: '100vh'
+        borderLeft: "2px solid #eee",
+        minHeight: "100vh",
       }}
     >
       {children}
     </div>
-  )
+  );
 }
 
 function Logo() {
@@ -79,12 +114,12 @@ function Logo() {
     <div
       style={{
         marginTop: 20,
-        marginBottom: 10
+        marginBottom: 10,
       }}
     >
       <a href="/">
         <img src={logo} height={64} width={64} alt="logo" />
       </a>
     </div>
-  )
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,94 @@
-import react from '@vitejs/plugin-react'
-import ssr from 'vite-plugin-ssr/plugin'
-import { UserConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import ssr from "vite-plugin-ssr/plugin";
+import { UserConfig, defineConfig } from "vite";
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
-const config: UserConfig = {
-  plugins: [react(), ssr()]
-}
+export default defineConfig({
+  resolve: {
+    // conditions: ['import', 'module', 'node', 'default'],
+    alias: {
+      // This Rollup aliases are extracted from @esbuild-plugins/node-modules-polyfill,
+      // see https://github.com/remorses/esbuild-plugins/blob/master/node-modules-polyfill/src/polyfills.ts
+      // process and buffer are excluded because already managed
+      // by node-globals-polyfill
+      util: "util",
+      sys: "util",
+      events: "rollup-plugin-node-polyfills/polyfills/events",
+      stream: "rollup-plugin-node-polyfills/polyfills/stream",
+      path: "rollup-plugin-node-polyfills/polyfills/path",
+      querystring: "rollup-plugin-node-polyfills/polyfills/qs",
+      punycode: "rollup-plugin-node-polyfills/polyfills/punycode",
+      url: "rollup-plugin-node-polyfills/polyfills/url",
+      // string_decoder: "rollup-plugin-node-polyfills/polyfills/string-decoder",
+      http: "rollup-plugin-node-polyfills/polyfills/http",
+      https: "rollup-plugin-node-polyfills/polyfills/http",
+      os: "rollup-plugin-node-polyfills/polyfills/os",
+      assert: "rollup-plugin-node-polyfills/polyfills/assert",
+      constants: "rollup-plugin-node-polyfills/polyfills/constants",
+      _stream_duplex:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
+      _stream_passthrough:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
+      _stream_readable:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
+      _stream_writable:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
+      _stream_transform:
+        "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
+      timers: "rollup-plugin-node-polyfills/polyfills/timers",
+      console: "rollup-plugin-node-polyfills/polyfills/console",
+      vm: "rollup-plugin-node-polyfills/polyfills/vm",
+      zlib: "rollup-plugin-node-polyfills/polyfills/zlib",
+      tty: "rollup-plugin-node-polyfills/polyfills/tty",
+      domain: "rollup-plugin-node-polyfills/polyfills/domain",
+    },
+  },
+  publicDir: path.resolve(__dirname, "../../public"),
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    ssr(),
+    {
+      name: "fix-node-globals-polyfill",
+      setup(build) {
+        build.onResolve({ filter: /util\.js/ }, ({ path }) => ({ path }));
+      },
+    },
+  ],
 
-export default config
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        // @ts-expect-error ...
+        rollupNodePolyFill(),
+      ],
+    },
+  },
+  ssr: {
+    target: "webworker",
+    noExternal: true,
+  },
+}) as UserConfig;
